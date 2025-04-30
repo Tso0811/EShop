9f2b984de56457c14b3473a014afda9b8d73d62a
Tso0811 <savagetso0811@gmail.com>
2025-04-30 21:13:31 +0800

    完成顯示商品細節

--- products/url.py
+++ products/url.py
@@ -4,4 +4,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('' , views.index_view , name='index_view'),
+    path('products/<int:id>/' , views.products_detail_view , name='products_detail_view'),
 ]
--- products/views.py
+++ products/views.py
@@ -8,4 +8,7 @@ def index_view(request):            #首頁
     product = Product.objects.all()
     return render(request , 'index.html' , {'products':product})
 
+def products_detail_view(request , id):
+    product = get_object_or_404(Product , pk = id)
+    return render (request , 'products_detail.html' , {'product':product})
 
