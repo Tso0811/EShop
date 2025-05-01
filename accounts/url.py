8f6e2ba9f573e9baa54f712acf70d5a80bb268f2
Tso0811 <savagetso0811@gmail.com>
2025-05-01 17:06:11 +0800

    新增accouts app

---
+++ accounts/url.py
@@ -0,0 +1,8 @@
+#products app url
+from django.contrib import admin
+from django.urls import path
+from . import views
+app_name = 'account'
+urlpatterns = [
+
+]
--- products/url.py
+++ products/url.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+app_name = 'products'
 urlpatterns = [
     path('' , views.index_view , name='index_view'),
-    path('products/' , views.products_view ,  ),
+    path('products/' , views.products_view , name='products_view'),
     path('products/<int:id>/' , views.products_detail_view , name='products_detail_view'),
 ]
