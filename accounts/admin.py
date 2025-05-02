ff2aa7b33ed0f5f66a1c874391d763ec1873c90a
Tso0811 <savagetso0811@gmail.com>
2025-05-02 23:51:28 +0800

    更改前端畫面 在後台管理新增顯示使用者電子郵件

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')  # 這裡加上 email 欄位
+
+admin.site.unregister(User)  # 先取消註冊原本的
+admin.site.register(User, UserAdmin)  # 用你自訂的顯示方式註冊
